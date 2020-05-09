5862e5f63cdbd4c6672bf5859dc95825d6ed24b9
swissalpine <a.may@mailbox.org>
2020-05-09 11:47:33 +0200

    Show avgDelta if n/a ("avgDelta --")

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewFragment.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewFragment.kt
@@ -550,9 +550,9 @@ class OverviewFragment : DaggerFragment(), View.OnClickListener, OnLongClickList
                 overview_deltashort?.text = Profile.toSignedUnitsString(glucoseStatus.delta, glucoseStatus.delta * Constants.MGDL_TO_MMOLL, units)
                 overview_avgdelta?.text = "avgΔ ${Profile.toUnitsString(glucoseStatus.short_avgdelta, glucoseStatus.short_avgdelta * Constants.MGDL_TO_MMOLL, units)}"
             } else {
-                overview_delta?.text = "Δ " + resourceHelper.gs(R.string.notavailable)
+                overview_delta?.text = "Δ --" //+ resourceHelper.gs(R.string.notavailable)
                 overview_deltashort?.text = "---"
-                overview_avgdelta?.text = ""
+                overview_avgdelta?.text = "avgΔ --"
             }
 
             // strike through if BG is old
