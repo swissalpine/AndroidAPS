6c9edb2052c6fe1b7b9e35dcbad14d298d7a357c
swissalpine <a.may@mailbox.org>
2020-05-11 21:20:04 +0200

    Try to fix double notification (local alert and system notification)

--- app/src/main/java/info/nightscout/androidaps/plugins/aps/loop/LoopPlugin.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/aps/loop/LoopPlugin.java
@@ -442,14 +442,14 @@ public class LoopPlugin extends PluginBase {
                             && resultAfterConstraints.carbsReq >= sp.getInt(R.string.key_smb_enable_carbs_suggestions_threshold, 0)
                             && carbsSuggestionsSuspendedUntil < System.currentTimeMillis()) {
 
-                        if (sp.getBoolean(R.string.key_enable_carbs_required_alert_local,true)) {
+                        if (sp.getBoolean(R.string.key_enable_carbs_required_alert_local,true) && !sp.getBoolean(R.string.key_raise_notifications_as_android_notifications, false)) {
                             Notification carbreqlocal = new Notification(Notification.CARBS_REQUIRED, resultAfterConstraints.getCarbsRequiredText(), Notification.NORMAL);
                             rxBus.send(new EventNewNotification(carbreqlocal));
                         }
                         if (sp.getBoolean(R.string.key_ns_create_announcements_from_carbs_req, false)) {
                             NSUpload.uploadError(resultAfterConstraints.getCarbsRequiredText());
                         }
-                        if (sp.getBoolean(R.string.key_raise_notifications_as_android_notifications, false)){
+                        if (sp.getBoolean(R.string.key_enable_carbs_required_alert_local,true) && sp.getBoolean(R.string.key_raise_notifications_as_android_notifications, false)){
                             Intent intentAction5m = new Intent(context, CarbSuggestionReceiver.class);
                             intentAction5m.putExtra("ignoreDuration", 5);
                             PendingIntent pendingIntent5m = PendingIntent.getBroadcast(context, 1, intentAction5m, PendingIntent.FLAG_UPDATE_CURRENT);
