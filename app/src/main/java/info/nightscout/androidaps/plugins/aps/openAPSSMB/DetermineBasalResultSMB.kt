217b2fa94e3855d781daaca6eee588f50f41f5f5
swissalpine <a.may@mailbox.org>
2021-07-01 17:08:09 +0200

    KetoAcidosis Protection and some string changes

--- app/src/main/java/info/nightscout/androidaps/plugins/aps/openAPSSMB/DetermineBasalResultSMB.kt
+++ app/src/main/java/info/nightscout/androidaps/plugins/aps/openAPSSMB/DetermineBasalResultSMB.kt
@@ -1,10 +1,16 @@
 package info.nightscout.androidaps.plugins.aps.openAPSSMB
 
+import info.nightscout.androidaps.R
 import dagger.android.HasAndroidInjector
+import info.nightscout.androidaps.events.Event
+import info.nightscout.androidaps.interfaces.IobCobCalculator
 import info.nightscout.androidaps.logging.LTag
 import info.nightscout.androidaps.plugins.aps.loop.APSResult
 import org.json.JSONException
 import org.json.JSONObject
+import javax.inject.Inject
+
+
 
 class DetermineBasalResultSMB private constructor(injector: HasAndroidInjector) : APSResult(injector) {
 
@@ -29,6 +35,25 @@ class DetermineBasalResultSMB private constructor(injector: HasAndroidInjector) 
                 tempBasalRequested = true
                 rate = result.getDouble("rate")
                 if (rate < 0.0) rate = 0.0
+
+                // Ketocidosis Protection
+                // Calculate IOB
+                val bolusIob = iobCobCalculator.calculateIobFromBolus()
+                val basalIob = iobCobCalculator.calculateIobFromTempBasalsIncludingConvertedExtended()
+                // Get active BaseBasalRate
+                val baseBasalRate = activePlugin.activePump.baseBasalRate
+                // Activate a small TBR
+                if (sp.getBoolean(R.string.key_keto_protect, false) && sp.getBoolean(R.string.key_variable_keto_protect_strategy, true) && bolusIob.iob + basalIob.basaliob < 0 - baseBasalRate && -(bolusIob.activity + basalIob.activity) > 0) {
+                    // Variable strategy
+                    val cutoff: Double = baseBasalRate * (sp.getDouble(R.string.keto_protect_basal, 20.0) * 0.01)
+                    if (rate < cutoff) rate = cutoff
+                } else if (sp.getBoolean(R.string.key_keto_protect, false) && !sp.getBoolean(R.string.key_variable_keto_protect_strategy, true)) {
+                    // Continuous strategy
+                    val cutoff: Double = baseBasalRate * (sp.getDouble(R.string.keto_protect_basal, 20.0) * 0.01)
+                    if (rate < cutoff) rate = cutoff
+                }
+                // End Ketoacidosis Protetion
+
                 duration = result.getInt("duration")
             } else {
                 rate = (-1).toDouble()
