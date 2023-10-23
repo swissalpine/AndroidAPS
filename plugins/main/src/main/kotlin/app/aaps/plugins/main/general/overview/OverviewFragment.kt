2626f06a993ffd1ca0a4103244eee7ea2b5afe86
swissalpine <a.may@mailbox.org>
2023-10-23 22:41:05 +0200

    Revert adaptions for exercise button

--- plugins/main/src/main/kotlin/app/aaps/plugins/main/general/overview/OverviewFragment.kt
+++ plugins/main/src/main/kotlin/app/aaps/plugins/main/general/overview/OverviewFragment.kt
@@ -188,37 +188,21 @@ class OverviewFragment : DaggerFragment(), View.OnClickListener, OnLongClickList
 
         // Mod exercise mode toggle icon
         if ( sp.getBoolean(app.aaps.core.utils.R.string.key_high_temptarget_raises_sensitivity, false)) {
-            setRibbon(
-                binding.exerciseModeCheckboxIcon,
-                app.aaps.core.ui.R.attr.ribbonTextWarningColor,
-                app.aaps.core.ui.R.attr.ribbonWarningColor,
-                "Exercise Mode: enabled")
-            binding.exerciseModeCheckboxIcon.setIconTintResource(app.aaps.core.ui.R.color.ribbonTextWarning)
+            binding.exerciseModeCheckboxIcon.setImageResource(R.drawable.exercise)
+            binding.exerciseModeCheckboxIcon.setBackgroundResource(app.aaps.core.ui.R.color.ribbonWarning)
         } else {
-            setRibbon(
-                binding.exerciseModeCheckboxIcon,
-                app.aaps.core.ui.R.attr.ribbonTextDefaultColor,
-                app.aaps.core.ui.R.attr.ribbonDefaultColor,
-                "Exercise Mode: disabled")
-            binding.exerciseModeCheckboxIcon.setIconTintResource(app.aaps.core.ui.R.color.ribbonTextDefault)
+            binding.exerciseModeCheckboxIcon.setImageResource(R.drawable.exerciseinactive)
+            binding.exerciseModeCheckboxIcon.setBackgroundResource(app.aaps.core.ui.R.color.ribbonDefault)
         }
         binding.exerciseModeCheckboxIcon.setOnClickListener {
-            if (sp.getBoolean(app.aaps.core.utils.R.string.key_high_temptarget_raises_sensitivity, false)) {
+            if (sp.getBoolean(app.aaps.core.utils.R.string.key_high_temptarget_raises_sensitivity, false) == true) {
+                binding.exerciseModeCheckboxIcon.setImageResource(R.drawable.exerciseinactive)
+                binding.exerciseModeCheckboxIcon.setBackgroundResource(app.aaps.core.ui.R.color.ribbonDefault)
                 sp.putBoolean(app.aaps.core.utils.R.string.key_high_temptarget_raises_sensitivity, false)
-                setRibbon(
-                    binding.exerciseModeCheckboxIcon,
-                    app.aaps.core.ui.R.attr.ribbonTextDefaultColor,
-                    app.aaps.core.ui.R.attr.ribbonDefaultColor,
-                    "Exercise Mode: disabled")
-                binding.exerciseModeCheckboxIcon.setIconTintResource(app.aaps.core.ui.R.color.ribbonTextDefault)
             } else {
+                binding.exerciseModeCheckboxIcon.setImageResource(R.drawable.exercise)
+                binding.exerciseModeCheckboxIcon.setBackgroundResource(app.aaps.core.ui.R.color.ribbonWarning)
                 sp.putBoolean(app.aaps.core.utils.R.string.key_high_temptarget_raises_sensitivity, true)
-                setRibbon(
-                    binding.exerciseModeCheckboxIcon,
-                    app.aaps.core.ui.R.attr.ribbonTextWarningColor,
-                    app.aaps.core.ui.R.attr.ribbonWarningColor,
-                    "Exercise Mode: enabled")
-                binding.exerciseModeCheckboxIcon.setIconTintResource(app.aaps.core.ui.R.color.ribbonTextWarning)
             }
         }
         // Mod end
