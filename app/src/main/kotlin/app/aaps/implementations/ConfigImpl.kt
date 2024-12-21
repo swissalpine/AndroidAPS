ddec21ccff6d303d4f685f502dc6ad1fd0e98540
swissalpine <a.may@mailbox.org>
2024-12-21 17:18:11 +0100

    Enable engineering mode

--- app/src/main/kotlin/app/aaps/implementations/ConfigImpl.kt
+++ app/src/main/kotlin/app/aaps/implementations/ConfigImpl.kt
@@ -42,7 +42,7 @@ class ConfigImpl @Inject constructor(
     private var doNotSendSmsOnProfileChange: Boolean? = null
 
     override fun isEngineeringModeOrRelease(): Boolean = if (!APS) true else isEngineeringMode() || !isDev()
-    override fun isEngineeringMode(): Boolean = isEngineeringMode ?: (fileListProvider.ensureExtraDirExists()?.findFile("engineering_mode") != null).also { isEngineeringMode = it }
+    override fun isEngineeringMode(): Boolean = true //isEngineeringMode ?: (fileListProvider.ensureExtraDirExists()?.findFile("engineering_mode") != null).also { isEngineeringMode = it }
     override fun isUnfinishedMode(): Boolean = isUnfinishedMode ?: (fileListProvider.ensureExtraDirExists()?.findFile("unfinished_mode") != null).also { isUnfinishedMode = it }
     override fun isDev(): Boolean = (VERSION.contains("-") || VERSION.matches(Regex(".*[a-zA-Z]+.*"))) && !VERSION.contains("-beta") && !VERSION.contains("-rc")
     override fun showUserActionsOnWatchOnly(): Boolean = showUserActionsOnWatchOnly ?: (fileListProvider.ensureExtraDirExists()?.findFile("show_user_actions_on_watch_only") != null).also { showUserActionsOnWatchOnly = it }
