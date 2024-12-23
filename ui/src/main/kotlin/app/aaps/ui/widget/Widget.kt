29f1541566f6e28b552522f9efcbdb7a48c885eb
swissalpine <a.may@mailbox.org>
2024-12-23 17:07:44 +0100

    Widget: modify display of profile and temp target

--- ui/src/main/kotlin/app/aaps/ui/widget/Widget.kt
+++ ui/src/main/kotlin/app/aaps/ui/widget/Widget.kt
@@ -226,7 +226,7 @@ class Widget : AppWidgetProvider() {
             views.setTextColor(R.id.temp_target, rh.gc(app.aaps.core.ui.R.color.widget_ribbonWarning))
             views.setTextViewText(
                 R.id.temp_target,
-                profileUtil.toTargetRangeString(tempTarget.lowTarget, tempTarget.highTarget, GlucoseUnit.MGDL, units) + " " + dateUtil.untilString(tempTarget.end, rh)
+                profileUtil.toTargetRangeString(tempTarget.lowTarget, tempTarget.highTarget, GlucoseUnit.MGDL, units) + " " + dateUtil.untilString(tempTarget.end, rh).replace("h ","h")
             )
         } else {
             // If the target is not the same as set in the profile then oref has overridden it
@@ -265,7 +265,7 @@ class Widget : AppWidgetProvider() {
                 }
             } ?: rh.gc(app.aaps.core.ui.R.color.widget_ribbonCritical)
 
-        views.setTextViewText(R.id.active_profile, profileFunction.getProfileNameWithRemainingTime())
+        views.setTextViewText(R.id.active_profile, profileFunction.getProfileNameWithRemainingTime().replace("h ","").replace("%)(","%|").replace("(", " ("))
         // this is crashing, use background as text for now
         //views.setInt(R.id.active_profile, "setBackgroundColor", profileBackgroundColor)
         //views.setTextColor(R.id.active_profile, profileTextColor)
