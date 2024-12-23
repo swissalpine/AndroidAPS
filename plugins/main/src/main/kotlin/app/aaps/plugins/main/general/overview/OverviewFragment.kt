5ea6e145c0d5939934284c4663a08ce345a4b0c3
swissalpine <a.may@mailbox.org>
2024-12-23 16:52:55 +0100

    Overview: show variable sens in mg/dl without floating point

--- plugins/main/src/main/kotlin/app/aaps/plugins/main/general/overview/OverviewFragment.kt
+++ plugins/main/src/main/kotlin/app/aaps/plugins/main/general/overview/OverviewFragment.kt
@@ -1147,10 +1147,15 @@ class OverviewFragment : DaggerFragment(), View.OnClickListener, OnLongClickList
             else 0.0
         val ratioUsed = request?.autosensResult?.ratio ?: 1.0
 
+        // mod mg/dl without floating point
+        var formatVariableSense = "%1$.0f→%2$.0f"
+        if (profileFunction.getUnits() == GlucoseUnit.MMOL)
+            formatVariableSense = "%1$.1f→%2$.1f"
+        // end mod
         if (variableSens != isfMgdl && variableSens != 0.0 && isfMgdl != null) {
             var text = if (ratioUsed != 1.0 && ratioUsed != lastAutosensData?.autosensResult?.ratio) String.format(Locale.getDefault(), "Alg: %.0f%%\n", ratioUsed * 100) else ""
             text += String.format(
-                Locale.getDefault(), "%1$.1f→%2$.1f (%3$.1f)",
+                Locale.getDefault(), formatVariableSense, //"%1$.1f→%2$.1f (%3$.1f)",
                 profileUtil.fromMgdlToUnits(isfMgdl, profileFunction.getUnits()),
                 profileUtil.fromMgdlToUnits(variableSens, profileFunction.getUnits()),
                 profileUtil.fromMgdlToUnits(isfForCarbs ?: 0.0, profileFunction.getUnits())
