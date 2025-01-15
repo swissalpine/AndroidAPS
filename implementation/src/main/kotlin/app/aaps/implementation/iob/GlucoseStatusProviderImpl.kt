3bd10483ff4beb219ed50d74fde3e105923b6fde
ga-zelle <ga.zelle@netcologne.de>
2025-01-15 19:39:52 +0100

    fix smb delivery lower limits

--- implementation/src/main/kotlin/app/aaps/implementation/iob/GlucoseStatusProviderImpl.kt
+++ implementation/src/main/kotlin/app/aaps/implementation/iob/GlucoseStatusProviderImpl.kt
@@ -181,7 +181,7 @@ class GlucoseStatusProviderImpl @Inject constructor(
                 if ( orig[0].timestamp - orig[2].timestamp < 3 * 60000 ) {
                     use1MinuteRaw = true
                     sizeRecords = orig.size
-                    fslMinDur = preferences.get(IntKey.FslMinFitMinutes)    //sp.getInt(R.string.key_fslMinFitMinutes, 20)
+                    fslMinDur = preferences.get(IntKey.FslMinFitMinutes)
                 }
             }
         }
